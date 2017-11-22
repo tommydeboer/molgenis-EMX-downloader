acef3d1d131665c9bb3cc38f55f568b96f28d7b5
tommydeboer <tommydeboer4@gmail.com>
2017-11-22 12:23:32 +0100

    Add export to TTL in RdfBackend

--- src/main/java/org/molgenis/downloader/rdf/RdfBackend.java
+++ src/main/java/org/molgenis/downloader/rdf/RdfBackend.java
@@ -1,20 +1,62 @@
 package org.molgenis.downloader.rdf;
 
+import com.google.common.io.Files;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.rio.turtle.TurtleWriter;
 import org.eclipse.rdf4j.sail.nativerdf.NativeStore;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Path;
 
-public class RdfBackend
+public class RdfBackend implements AutoCloseable
 {
-	public Repository createRepository(Path path)
+	private static final String INDEXES = "spoc,posc,cosp";
+	private final Repository repository;
+	private final File outputPath;
+	private final File storeDir;
+
+	public RdfBackend(Path path, boolean overwrite) throws FileAlreadyExistsException
 	{
-		File dataDir = path.toFile();
-		String indexes = "spoc,posc,cosp";
-		Repository repo = new SailRepository(new NativeStore(dataDir, indexes));
-		repo.initialize();
-		return repo;
+		if (path.toFile().exists() && !overwrite)
+		{
+			throw new FileAlreadyExistsException(path.toString(), null, "File already exists.");
+		}
+
+		this.outputPath = path.toFile();
+
+		storeDir = Files.createTempDir();
+		storeDir.deleteOnExit();
+
+		repository = createRepository();
+	}
+
+	private Repository createRepository()
+	{
+		Repository sailRepository = new SailRepository(new NativeStore(storeDir, INDEXES));
+		sailRepository.initialize();
+		return sailRepository;
+	}
+
+	public Repository getRepository()
+	{
+		return repository;
+	}
+
+	public void export() throws IOException
+	{
+		try (FileOutputStream out = new FileOutputStream(outputPath))
+		{
+			repository.getConnection().export(new TurtleWriter(out));
+		}
+	}
+
+	@Override
+	public void close() throws Exception
+	{
+		repository.shutDown();
 	}
 }
